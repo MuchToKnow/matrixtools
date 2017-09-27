a3af45fe77458a17425c9fb60cab5c3aac5007f0
NotMuchToLearn <firepower789@gmail.com>
2017-09-26 19:49:43 -0700

    Initial Commit, work in progress.

---
+++ Matrix Project/src/matrices/Matrix.java
@@ -0,0 +1,42 @@
+package matrices;
+
+public class Matrix {
+	
+	private int cols;
+	private int rows;
+	private int[][] matrix;
+	
+	//Creates a matrix of rows x cols with values mrx.
+	public Matrix(int[][] mrx, int rows, int cols) {
+		this.matrix = mrx;
+		this.rows = rows;
+		this.cols = cols;
+	}
+	
+	public int[][] getMatrix() {
+		return this.matrix;
+	}
+	
+	public int getCols() {
+		return this.cols;
+	}
+	
+	public int getRows() {
+		return this.rows;
+	}
+	
+	public int[][] addMatrix(Matrix m){
+		int[][] ret = new int[rows][cols];
+		for(int r = 0; r < m.rows; r++) {
+			for(int c = 0; c < m.cols; c++) {
+				ret[r][c] = m.getMatrix()[r][c] + this.matrix[r][c];
+			}
+		}
+		return ret;
+	}
+	
+	public int[][] multiplyMatrix(Matrix m){
+		return this.matrix;
+	}
+
+}
