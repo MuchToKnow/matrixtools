7610fb22bd6d765b8c868ec83962a77b627405bd
NotMuchToLearn <firepower789@gmail.com>
2017-09-26 21:42:48 -0700

    Added addMatrix function and printMatrix function.

--- Matrix Project/src/matrices/Matrix.java
+++ Matrix Project/src/matrices/Matrix.java
@@ -1,5 +1,9 @@
 package matrices;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Matrix {
 	
 	private int cols;
@@ -7,10 +11,24 @@ public class Matrix {
 	private int[][] matrix;
 	
 	//Creates a matrix of rows x cols with values mrx.
-	public Matrix(int[][] mrx, int rows, int cols) {
+	public Matrix(int[][] mrx) {
 		this.matrix = mrx;
+		this.rows = mrx.length;
+		this.cols = mrx[0].length;
+	}
+	
+	public Matrix(int rows, int cols) throws NumberFormatException, IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		this.rows = rows;
 		this.cols = cols;
+		this.matrix = new int[rows][cols];
+		for(int r = 0; r < rows; r++) {
+			for(int c = 0; c < cols; c++) {
+				System.out.println("Entry (" + r + ", " + c + "):" );
+				this.matrix[r][c] = Integer.parseInt(reader.readLine());
+			}
+		}
+		
 	}
 	
 	public int[][] getMatrix() {
@@ -25,19 +43,39 @@ public class Matrix {
 		return this.rows;
 	}
 	
-	public int[][] addMatrix(Matrix m){
+	//Prints a Matrix to the console
+	public void printMatrix() {
+		for(int r = 0; r < this.rows; r++) {
+			for(int c = 0; c < this.cols; c++) {
+				System.out.print(this.matrix[r][c] + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	//Adds two matrices of the same size
+	public Matrix addMatrix(Matrix m){
 		int[][] ret = new int[rows][cols];
+		//check for same size
+		if ((m.cols != this.cols) || (m.rows != this.rows)) {
+			return new Matrix(ret);
+		}
 		for(int r = 0; r < m.rows; r++) {
 			for(int c = 0; c < m.cols; c++) {
 				ret[r][c] = m.getMatrix()[r][c] + this.matrix[r][c];
 			}
 		}
-		return ret;
+		return new Matrix(ret);
 	}
 	
-	public int[][] multiplyMatrix(Matrix m){
-		// TODO add method
-		return this.matrix;
+	//Multiplies this by m provided m has dimensions: ([this.cols][this.rows])
+	public Matrix multiplyMatrix(Matrix m){
+		int[][] ret = new int[this.rows][m.cols];
+		for(int r = 0; r < this.rows; r++) {
+			for(int c = 0; c < m.cols; c++) {
+				
+			}
+		}
+		return new Matrix(ret);
 	}
-
 }
--- Matrix Project/src/matrices/MatrixTools.java
+++ Matrix Project/src/matrices/MatrixTools.java
@@ -1,8 +1,12 @@
 package matrices;
 
+import java.io.IOException;
+
 public class MatrixTools {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		Matrix m1 = new Matrix(5,5);
+		Matrix m2 = new Matrix(5,5);
+		m1.addMatrix(m2).printMatrix();
 	}
-
 }
